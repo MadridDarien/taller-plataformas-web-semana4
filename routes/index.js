80f7c85df0bc653a52508b64bafa252fcb0e7ce8
MadridDarien <darieenmadridsalazar17@gmail.com>
2026-04-10 14:21:51 -0400

    Agrega ruta api de viajes

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,14 @@ router.post('/login', (req, res) => {
     }
 });
 
+router.get('/api/viajes', (req, res) => {
+    const viajes = [
+        { id: 1, destino: 'París', dias: 7 },
+        { id: 2, destino: 'Puerto Montt', dias: 4 },
+        { id: 3, destino: 'Nueva York', dias: 6 }
+    ];
+
+    res.json(viajes);
+});
+
 module.exports = router;
