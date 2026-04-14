4aea0dbef201442440cb7d110cb0110594e3ce21
diegobarahonab-ctrl <diego.barahona.b@gmail.com>
2026-04-13 22:36:00 -0300

    Enhance app.js with cookie parser and auth routes
    Added cookie parser middleware and authentication routes.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 3000;
 
-const rutas = require('./routes');
+require("dotenv").config(); // leer variables de entorno
 
+// Middlewares globales
 app.use(express.json());
+app.use(cookieParser()); // <-- NECESARIO para leer cookies
 
+// Rutas antiguas (las de tu compañero)
+const rutas = require('./routes');
 app.use('/', rutas);
+
+// Rutas nuevas de autenticación
+const authRoutes = require("./routes/authRoutes");
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
